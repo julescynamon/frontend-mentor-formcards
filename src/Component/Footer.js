707bb7959cd801efd037bd35f237d6d0028e8bb1
cynamon jules <julescweb@mail.com>
2022-09-02 08:39:44 +0200

    mise en place

---
+++ src/Component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./footer.scss";
+
+export default function Footer() {
+    return (
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+            Coded by <a href="https://julescynamon.github.io/cv_portfolio/">Jules Cynamon</a>.
+        </div>
+    )
+}
